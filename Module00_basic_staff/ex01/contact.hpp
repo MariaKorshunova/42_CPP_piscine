f930b161c270fe2bb8a0ba2913b6953447563ced
Jone Mabel <jmabel@student.21-school.ru>
2022-11-16 14:55:53 +0300

    day00ex00

---
+++ Module00_basic_staff/ex01/contact.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmabel <jmabel@student.21-school.ru>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/16 14:31:22 by jmabel            #+#    #+#             */
+/*   Updated: 2022/11/16 14:55:22 by jmabel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+#include <iostream>
+
+class Contact
+{
+private:
+    std::string firstName;
+    std::string lastName;
+    std::string nickName;
+    std::string phoneNumber;
+    std::string darkestSecret;
+
+public:
+    Contact();
+    ~Contact();
+};
+
+#endif
